949c89cafa369f7da66a3c22b0839e01ae2a2f67
voccer <duc.nguyen@pionero.io>
2022-11-18 14:05:17 +0700

    to-do list app

--- first-app/src/App.tsx
+++ first-app/src/App.tsx
@@ -1,26 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Image from 'next/image'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type ButtonProps = {
+  count: number
+  clickFunc: () => void
 }
 
-export default App;
+type ChangeProfileButtonProps = {
+  profileFlag: boolean
+  changeProfileFunc: () => void
+}
+
+function MyButtonAsc({
+  count,
+  clickFunc,
+}: React.PropsWithChildren<ButtonProps>): JSX.Element {
+  return <button onClick={clickFunc}>Click {count} times</button>
+}
+
+function MyButtonDesc({
+  count,
+  clickFunc,
+}: React.PropsWithChildren<ButtonProps>): JSX.Element {
+  return <button onClick={clickFunc}>Click {count} times desc</button>
+}
+
+function App() {
+  let initCount = localStorage.getItem('count')
+
+  const [count, setCount] = React.useState(initCount ? parseInt(initCount) : 0)
+  const [profileFlag, setProfileFlag] = React.useState(false)
+
+  function handleClick() {
+    setCount(count + 1)
+    localStorage.setItem('count', (count + 1).toString())
+  }
+
+  function handleClickDesc() {
+    setCount(count - 1)
+    localStorage.setItem('count', (count - 1).toString())
+  }
+
+  function changeProfileFunc() {
+    setProfileFlag(!profileFlag)
+  }
+
+  return (
+    <>
+      <h1>My first React app</h1>
+      <MyButtonAsc count={count} clickFunc={handleClick} />
+      <MyButtonAsc count={count} clickFunc={handleClick} />
+      <MyButtonDesc count={count} clickFunc={handleClickDesc} />
+      <br />
+      <ChangeProfileButton
+        profileFlag={profileFlag}
+        changeProfileFunc={changeProfileFunc}
+      >
+        {profileFlag ? <Profile1 /> : <Profile2 />}
+      </ChangeProfileButton>
+    </>
+  )
+}
+
+const ChangeProfileButton = ({
+  profileFlag,
+  changeProfileFunc,
+  children,
+}: React.PropsWithChildren<ChangeProfileButtonProps>): JSX.Element => {
+  return (
+    <>
+      <button onClick={changeProfileFunc}>
+        {' '}
+        {profileFlag ? 'Profile1' : 'Profile2'}
+      </button>
+      {children}
+    </>
+  )
+}
+
+const Profile1 = () => {
+  return (
+    <>
+      <Image />
+    </>
+  )
+  return (
+    <img
+      src='https://dummyimage.com/600x400/1a00ff/00ff6a'
+      alt='Katherine Johnson'
+      width={100}
+      height={100}
+    />
+  )
+}
+
+const Profile2 = () => {
+  return (
+    <img
+      src='https://dummyimage.com/600x400/000/fff'
+      alt='Katherine Johnson'
+      width={100}
+      height={100}
+    />
+  )
+}
+
+export default App
